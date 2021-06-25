367abc82382d0a9b2415a3984584e0787d37d4ce
FluffyMak4ic <57257635+FluffyMak4ic@users.noreply.github.com>
2021-06-25 15:42:30 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,5 +10,5 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 echo "Set config"
-cp .zshrc ~/.zshrc
-cp .config ~/.config
+cp -r .zshrc ~/.zshrc
+cp -r .config ~/
